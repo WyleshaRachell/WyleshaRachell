f32ae45adfedeb6c3f893ed2aac4bff607d67438
Wylesha Rachell <wfield1@gmail.com>
2017-10-17 21:42:45 -0400

    Class 9

---
+++ 08/relaxr/js/index.js
@@ -0,0 +1,47 @@
+//Stop Links from jumping on page
+var linkElements = document.getElementsByTagName('a');
+
+for (var i=0; i<linkElements.length; i++) {
+  linkElements[i].onclick = function(event) {
+    event.preventDefault();
+  }
+}
+
+
+//Toggle between readmore and readless
+var readMoreElement = document.getElementsByClassName('readmore');
+var readLessElement = document.getElementsByClassName('readless');
+var showThisElement = document.getElementById('show-this-on-click');
+
+readMoreElement[0].onclick = function() {
+  readMoreElement[0].style.display = 'none';
+  readLessElement[0].style.display = 'block';
+  showThisElement.style.display = 'block';
+};
+
+readLessElement[0].onclick = function() {
+  readMoreElement[0].style.display = 'block';
+  readLessElement[0].style.display = 'none';
+  showThisElement.style.display = 'none';
+};
+
+//Show Learn More text
+var learnMoreElement = document.getElementsByClassName('learnmore');
+var learnMoreTextElement = document.getElementById('learnmoretext');
+
+
+learnMoreElement[0].onclick = function(event) {
+  learnMoreElement[0].style.display = 'none';
+  learnMoreTextElement.style.display = 'block';
+  event.preventDefault();
+}
+
+
+
+
+
+
+
+
+
+
